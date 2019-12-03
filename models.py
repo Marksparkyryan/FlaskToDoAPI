5371f4624511e78abd27ce835046e5e6dc7b0c72
Mark Ryan <markryandeveloper@gmail.com>
2019-12-03 17:32:48 -0500

    added user model with create and password hasher methods

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from argon2 import PasswordHasher
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
                           BadSignature, SignatureExpired)
 from peewee import *
@@ -7,6 +8,39 @@ from peewee import *
 import config
 
 DATABASE = SqliteDatabase('todos.sqlite')
+HASHER = 
+
+class User(Model):
+    """Schema describing a user instance
+    """
+    username = CharField(max_length=12, unique=True)
+    email = CharField(unique=True)
+    password = CharField()
+
+    class Meta:
+        database = DATABASE
+    
+    @classmethod
+    def create_user(cls, username, email, password, **kwargs):
+        email = email.lower()
+        try:
+            cls.select().where(
+                (cls.email==email)|(cls.username**username)
+            ).get()
+        except cls.DoesNotExist:
+            user = cls(username=username, email=email)
+            user.password = user.set_password(password)
+            user.save()
+            return user
+        else:
+            raise Exception("User with that email or username already exists")
+    
+    @staticmethod
+    def set_password(password):
+        return HASHER.hash(password)
+    
+    def verify_password(self, password):
+        return HASHER.verify(self.password, password)
 
 
 class Todo(Model):
@@ -14,6 +48,10 @@ class Todo(Model):
     """
     title = CharField(max_length=256, unique=True)
     completed = BooleanField(default=False)
+    created_by = ForeignKeyField(User, related_name='todo_set')
+
+    class Meta:
+        database = DATABASE
 
 
 def initialize():
